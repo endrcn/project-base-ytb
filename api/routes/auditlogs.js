0fefe2be326446a5be8e0028b3f0d19eefc96338
endrcnn <endercan34@gmail.com>
2023-05-10 23:18:17 +0300

    #2 fix

--- api/routes/auditlogs.js
+++ api/routes/auditlogs.js
@@ -13,11 +13,11 @@ router.post("/", async (req, res) => {
         let skip = body.skip;
         let limit = body.limit;
 
-        if (typeof body.skip !== "numeric") {
+        if (typeof body.skip !== "number") {
             skip = 0;
         }
 
-        if (typeof body.limit !== "numeric" || body.limit > 500) {
+        if (typeof body.limit !== "number" || body.limit > 500) {
             limit = 500;
         }
 
